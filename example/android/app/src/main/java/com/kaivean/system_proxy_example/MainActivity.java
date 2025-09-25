55574510e2797d6f8c81e4641f1e1b89ea066dce
Yassine Ben Massaoud <48437666+UnluckyY1@users.noreply.github.com>
2025-09-25 17:33:50 +0100

    refactor: remove Android and iOS plugin implementations and related files

--- example/android/app/src/main/java/com/kaivean/system_proxy_example/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.kaivean.system_proxy_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity { }
--- ios/Classes/SystemProxyPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "SystemProxyPlugin.h"
-#import <system_proxy/system_proxy-Swift.h>
-
-@implementation SystemProxyPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftSystemProxyPlugin registerWithRegistrar:registrar];
-}
-@end
